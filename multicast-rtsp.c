534860ab25284b96b47d2b3a2e0a97c6f998a0d0
josh minor <josh.minor@arm.com>
2020-07-15 16:11:42 -0600

    Use videoconvert

--- multicast-rtsp.c
+++ multicast-rtsp.c
@@ -88,17 +88,17 @@ main (int argc, char *argv[])
    * any launch line works as long as it contains elements named pay%d. Each
    * element with pay%d names will be a stream */
   factory = gst_rtsp_media_factory_new ();
-//   gst_rtsp_media_factory_set_launch (factory, "( "
-//       "v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480,framerate=15/1 ! "
-//       "x264enc ! rtph264pay name=pay0 pt=96 "
-//       "audiotestsrc ! audio/x-raw,rate=8000 ! "
-//       "alawenc ! rtppcmapay name=pay1 pt=97 " ")");
-
   gst_rtsp_media_factory_set_launch (factory, "( "
-      "videotestsrc ! x264enc ! rtph264pay name=pay0 pt=96 ! "
+      "v4l2src device=/dev/video0 ! videoconvert ! "
       "x264enc ! rtph264pay name=pay0 pt=96 "
       "audiotestsrc ! audio/x-raw,rate=8000 ! "
       "alawenc ! rtppcmapay name=pay1 pt=97 " ")");
+
+//   gst_rtsp_media_factory_set_launch (factory, "( "
+//       "videotestsrc ! x264enc ! rtph264pay name=pay0 pt=96 ! "
+//       "x264enc ! rtph264pay name=pay0 pt=96 "
+//       "audiotestsrc ! audio/x-raw,rate=8000 ! "
+//       "alawenc ! rtppcmapay name=pay1 pt=97 " ")");
 
   gst_rtsp_media_factory_set_shared (factory, TRUE);
 
